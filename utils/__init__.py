0934c965a65422d99c4e09d6aad85c35ed2b2bed
Anish Kamatam <anishkamatam22@gmail.com>
2026-02-03 14:33:34 -0800

    Add Tokenizer

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,5 @@
 from .sampling_params import SamplingParams
 from .config import ModelConfig
+from .tokenizer import Tokenizer
 
-__all__ = ["SamplingParams", "ModelConfig"]
+__all__ = ["SamplingParams", "ModelConfig", "Tokenizer"]
---
+++ utils/tokenizer.py
@@ -0,0 +1,36 @@
+from typing import List, Optional
+from transformers import AutoTokenizer
+
+
+class Tokenizer:
+    def __init__(self, model_path: str):
+        self._tokenizer = AutoTokenizer.from_pretrained(
+            model_path,
+            trust_remote_code=True,
+        )
+
+    def encode(self, text: str, add_special_tokens: bool = True) -> List[int]:
+        return self._tokenizer.encode(text, add_special_tokens=add_special_tokens)
+
+    def decode(self, token_ids: List[int], skip_special_tokens: bool = True) -> str:
+        return self._tokenizer.decode(token_ids, skip_special_tokens=skip_special_tokens)
+
+    @property
+    def vocab_size(self) -> int:
+        return len(self._tokenizer)
+
+    @property
+    def eos_token_id(self) -> Optional[int]:
+        return self._tokenizer.eos_token_id
+
+    @property
+    def bos_token_id(self) -> Optional[int]:
+        return self._tokenizer.bos_token_id
+
+    @property
+    def pad_token_id(self) -> int:
+        return (
+            self._tokenizer.pad_token_id
+            if self._tokenizer.pad_token_id is not None
+            else self._tokenizer.eos_token_id
+        )
